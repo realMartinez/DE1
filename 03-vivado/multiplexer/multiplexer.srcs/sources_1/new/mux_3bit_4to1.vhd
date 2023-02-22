38a508a22b27597ee61d3f41efd5a6294e92137f
Olšiak Martin (222051) <xolsia00@vutbr.cz>
2023-02-22 10:47:26 +0100

    end of lab commit

--- 03-vivado/multiplexer/multiplexer.srcs/sources_1/new/mux_3bit_4to1.vhd
+++ 03-vivado/multiplexer/multiplexer.srcs/sources_1/new/mux_3bit_4to1.vhd
@@ -4,21 +4,29 @@ use ieee.std_logic_1164.all;
 ------------------------------------------------------------
 -- Entity declaration for mux_3bit_4to1
 ------------------------------------------------------------
-entity comparator_2bit is
+entity mux_3bit_4to1 is
     port(
-        b_i           : in  std_logic_vector(1 downto 0);
-        a_i           : in  std_logic_vector(1 downto 0);
-        B_less_A_o    : out std_logic  -- B is less than A
+        sel           : in  std_logic_vector(1 downto 0);
+        f_o           : in  std_logic_vector(2 downto 0);
+        d_i           : in  std_logic_vector(2 downto 0);
+        c_i           : in  std_logic_vector(2 downto 0);
+        b_i           : in  std_logic_vector(2 downto 0);
+        a_i           : in  std_logic_vector(2 downto 0)
     );
-end entity comparator_2bit;
+end entity mux_3bit_4to1;
 
 ------------------------------------------------------------
 -- Architecture body for 2-bit binary comparator
 ------------------------------------------------------------
-architecture Behavioral of comparator_2bit is
+architecture Behavioral of mux_3bit_4to1 is
 begin
---    B_greater_A_o <= -- WRITE "GREATER" ASSIGNMENT HERE
---    B_equals_A_o  <= -- WRITE "EQUALS" ASSIGNMENT HERE
-    B_less_A_o    <= '1' when (b_i < a_i) else '0';
+
+case sel is 
+    when "00" => f_o <= a_i;
+    when "01" => f_o <= b_i;
+    when "10" => f_o <= c_i;
+    when "11" => f_o <= d_i;
+end case;
+
 
 end architecture Behavioral;
